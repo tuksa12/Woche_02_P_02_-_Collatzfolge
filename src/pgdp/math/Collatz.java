8bc506bdeda1a3b0bd14b179cde4ee4811f83133
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-11-20 00:52:52 +0100

    00:53

--- src/pgdp/math/Collatz.java
+++ src/pgdp/math/Collatz.java
@@ -6,21 +6,21 @@ public class Collatz {
 	public static void main(String[] args) {
 		int n = readInt("Bitte Zahl eingeben:");
 		int t = 1;
-		if (n <= 0) {
-			writeLineConsole("Fehler: n>0 erwartet!");
-		}
-		while (n !=1){
-			writeConsole(n+" ");
-			if (n%2==0){
-				n = n/2;
-			}
-			else {
-				n = (3 * n) + 1;
-			}
-			t++;
+		if (n == 0) {
+			write("Fehler: n>0 erwartet!");
+		}else{
+			while (n > 1){
+				writeConsole(n+" ");
+				if (n%2==0){
+					n = n/2;
+				}
+				else {
+					n = (3 * n) + 1;
+				}
+				t++;
 
-		}
-		writeLineConsole("1");
-		write("Länge: " +t);
+			}
+			writeLineConsole("1");
+			write("Länge: " +t);}
 	}
 }
